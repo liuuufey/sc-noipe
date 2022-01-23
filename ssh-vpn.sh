9b6d4c0045d4308188bc65b6888422400296b417
Liuuufey <89542179+liuuufey@users.noreply.github.com>
2022-01-23 09:37:47 +0700

    Update ssh-vpn.sh

--- ssh-vpn.sh
+++ ssh-vpn.sh
@@ -331,6 +331,7 @@ wget -O port-sstp "https://raw.githubusercontent.com/liuuufey/sc-noip/main/port-
 wget -O port-squid "https://raw.githubusercontent.com/liuuufey/sc-noip/main/port-squid.sh"
 wget -O port-ws "https://raw.githubusercontent.com/liuuufey/sc-noip/main/port-ws.sh"
 wget -O port-vless "https://raw.githubusercontent.com/liuuufey/sc-noip/main/port-vless.sh"
+wget -O port-sslh https://raw.githubusercontent.com/liuuufey/sc-noipe/main/port-sslh.sh
 wget -O wbmn "https://raw.githubusercontent.com/liuuufey/sc-noip/main/webmin.sh"
 wget -O xp "https://raw.githubusercontent.com/liuuufey/sc-noip/main/xp.sh"
 wget -O swap "https://raw.githubusercontent.com/liuuufey/sc-noip/main/swapkvm.sh"
@@ -378,6 +379,7 @@ chmod +x port-sstp
 chmod +x port-squid
 chmod +x port-ws
 chmod +x port-vless
+chmod +x port-sslh
 chmod +x wbmn
 chmod +x xp
 chmod +x swap
